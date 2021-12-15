366113e68e38426e06f344185327127d6c18d93e
Raminel Nunez <Raminelnc23@gmail.com>
2021-12-15 10:22:29 -0600

    added getBusRoute function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,7 @@
 const token = "pk.eyJ1IjoicmFtaW5lbG51bmV6IiwiYSI6ImNreDZzcWt5ZTA1Znkyb254MWZxdGk0ODUifQ.rDi0UeZg7jDeR8_ryr7FuA";
+const apiKey = "1vbBsFqGS6MWA0hvpBq0";
+const winnipegTransit = "https://api.winnipegtransit.com/v3/trip-planner.json?";
+const mapBox = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 
 const coords = {
   latitude: 0,
@@ -17,9 +20,9 @@ navigator.geolocation.getCurrentPosition((position) => {
   coords.longitude = position.coords.longitude;
 });
 
-const getGeocode = async function (qString) {
-  const targetUrl =`https://api.mapbox.com/geocoding/v5/mapbox.places/${qString}.json?types=address&access_token=${token}&proximity=${coords.longitude},${coords.latitude}&types=poi`
-  const response = await fetch(targetUrl);
+async function getGeocode (qString) {
+  const targetURL =`${mapBox}${qString}.json?types=address&access_token=${token}&proximity=${coords.longitude},${coords.latitude}&types=poi`
+  const response = await fetch(targetURL);
   const data = await response.json();
   return data.features;
 };
@@ -48,11 +51,19 @@ async function search(search_text) {
   }
 }
 
-
 class SearchResult {
   constructor(name, address, coords) {
     this.name = name;
     this.address = address;
     this.coords = coords;
   }
+}
+
+async function getBusRoute(origin, destination) {
+  let orig = origin.replace(/ /g, '&');
+  let dest = destination.replace(/ /g, '&');
+  const targetURL = `${winnipegTransit}api-key=${apiKey}&origin=addresses/${orig}&destination=addresses/${dest}`
+  const response = await fetch(targetURL);
+  const data = await response.json();
+  console.log(data)
 }
