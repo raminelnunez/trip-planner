216e6b2e8740e15f25d35ffe1a7747773f32f890
Raminel Nunez <Raminelnc23@gmail.com>
2021-12-15 14:48:19 -0600

    add working code without html rendering

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 const token = "pk.eyJ1IjoicmFtaW5lbG51bmV6IiwiYSI6ImNreDZzcWt5ZTA1Znkyb254MWZxdGk0ODUifQ.rDi0UeZg7jDeR8_ryr7FuA";
 const apiKey = "1vbBsFqGS6MWA0hvpBq0";
-const winnipegTransit = "https://api.winnipegtransit.com/v3/trip-planner.json?";
+const winnipegTransit = "https://api.winnipegtransit.com/v3/";
 const mapBox = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 
 const coords = {
@@ -13,12 +13,15 @@ const coords = {
   }
 };
 
-let results;
-
 navigator.geolocation.getCurrentPosition((position) => {
   coords.latitude = position.coords.latitude;
   coords.longitude = position.coords.longitude;
 });
+
+const info = {
+  origins: [],
+  destinations: [],
+}
 
 async function getGeocode (qString) {
   const targetURL =`${mapBox}${qString}.json?types=address&access_token=${token}&proximity=${coords.longitude},${coords.latitude}&types=poi`
@@ -27,43 +30,101 @@ async function getGeocode (qString) {
   return data.features;
 };
 
-const getDistance = function (arr) {
-  const [long, lat] = arr;
-  const longDiffKM = Math.abs(long - coords.longitude) * 111.319;
-  const latDiffKM = Math.abs(lat - coords.latitude) * 111.319;
-  const distance = Math.sqrt(longDiffKM ** 2 + latDiffKM ** 2);
-
-  return distance;
-};
-
-async function search(search_text) {
+async function search(search_text, location) {
   const searchResults = await getGeocode(search_text);
-  console.log(searchResults)
-  results = [];
+  let results = [];
   for (let item of searchResults) {
-    let info = item.place_name.split(', ')
-    let name = info[0];
-    let address = info[1];
+    let itemInfo = item.place_name.split(', ')
+    let name = itemInfo[0];
+    let address = itemInfo[1];
     let coords = item.geometry.coordinates
-    if (info.includes('Winnipeg') === true) {
-      results.push(new SearchResult(name, address, coords))
+    let key = await getKey(coords.reverse());
+    if (itemInfo.includes('Winnipeg') === true) {
+      results.push(new Result(name, address, coords, key))
     }
+  }
+  if (location === 'origin') {
+    info.origins = results;
+  }
+  if (location === 'destination') {
+    info.destinations = results;
   }
 }
 
-class SearchResult {
-  constructor(name, address, coords) {
+class Result {
+  constructor(name, address, coords, key) {
     this.name = name;
     this.address = address;
     this.coords = coords;
+    this.key = key;
   }
 }
 
-async function getBusRoute(origin, destination) {
-  let orig = origin.replace(/ /g, '&');
-  let dest = destination.replace(/ /g, '&');
-  const targetURL = `${winnipegTransit}api-key=${apiKey}&origin=addresses/${orig}&destination=addresses/${dest}`
+class Walk {
+  constructor(duration, destination) {
+    this.message = `Walk for ${duration} minutes to ${destination[0]}${destination[1]}${destination[2]}${destination[3]}`;
+  }
+}
+
+class Ride {
+  constructor (route, duration) {
+    this.message = `Ride the ${route} for ${duration} minutes`;
+  }
+}
+
+class Transfer {
+  constructor(origin, destination) {
+    this.message = `Transfer from stop #${origin[0]} - ${origin[1]} to stop #${destination[0]} - ${destination[1]}`
+  }
+}
+
+async function getKey(coords) {
+  const targetURL = `${winnipegTransit}locations.json?api-key=${apiKey}&lat=${coords[0]}&lon=${coords[1]}`;
   const response = await fetch(targetURL);
   const data = await response.json();
-  console.log(data)
+  if (data.locations.length > 0) {
+    if (data.locations[0].type === "address") {
+      return data.locations[0].key;
+    }
+    if (data.locations[0].type === "monument") {
+      return data.locations[0].address.key;
+    }
+  } else {
+    return 'some error';
+  }
+}
+
+async function getRoute(origKey, destKey) {
+  const targetURL = `${winnipegTransit}trip-planner.json?api-key=${apiKey}&origin=addresses/${origKey}&destination=addresses/${destKey}`
+  const response = await fetch(targetURL);
+  const data = await response.json();
+  parseRoute(data);
+}
+
+async function parseRoute(data) {
+  let routes = []
+  for (let plan of data.plans) {
+    let route = [];
+    let i = 0
+    for (let segment of plan.segments) {
+      i++
+      if (segment.type === 'walk') {
+        if (i === plan.segments.length) {
+          route.push(new Walk(segment.times.durations.total, ["your ", "destination.", "", ""] ));
+        } else if (i < plan.segments.length) {
+          route.push(new Walk(segment.times.durations.total, ["stop #", segment.to.stop.key, " - ",segment.to.stop.name]));
+        }
+      }
+      if (segment.type === 'ride') {
+        route.push(new Ride(segment.route.name, segment.times.durations.total));
+      }
+      if (segment.type === 'transfer') {
+        route.push(new Transfer([segment.from.stop.key, segment.from.stop.name], [segment.to.stop.key, segment.to.stop.name] ));
+      }
+    }
+    if (route[0] !== undefined) {
+      routes.push(route)
+    } 
+  }
+  console.log(routes)
 }
